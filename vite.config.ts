09a0a7405bf3877090d89d6134e325b24ff5ea17
sotamorita <souta.kiro.02@gmail.com>
2025-11-30 11:59:45 +0900

    fix: vite.configにbase pathを追加

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/kakutei-todo-app/',
+
   server: {
     port: 3000,
     host: '0.0.0.0',
